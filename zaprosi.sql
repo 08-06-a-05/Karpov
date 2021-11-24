110002a977ff730edf40183140925eb49a14dfdb
08-06-a-05 <nab.txt@mail.ru>
2021-11-24 13:21:58 +0300

    Update zaprosi.sql

--- zaprosi.sql
+++ zaprosi.sql
@@ -1,6 +1,6 @@
 UPDATE cars
 	SET year = substr(year,1,4)
-//Год до применения команды выше был указан в формате YYYY-MM-DD, но т.к. для автомобиля в основном важен год, то он был преобразован в формат YYYY
+--Год до применения команды выше был указан в формате YYYY-MM-DD, но т.к. для автомобиля в основном важен год, то он был преобразован в формат YYYY
 SELECT name_of_customer, MAX(id_order) AS 'last_user' FROM orders
 SELECT year FROM cars GROUP BY year
 SELECT COUNT(*) as 'cars_total' FROM cars
