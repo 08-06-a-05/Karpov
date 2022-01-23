41ae61bc1837a03b28f4eed86214ef035c9d6397
08-06-a-05 <nab.txt@mail.ru>
2022-01-23 18:27:33 +0300

    filled bd

--- work_with_flask.py
+++ work_with_flask.py
@@ -26,27 +26,39 @@ class Cars(db.Model):
     brand_connection = db.relationship('Brands', backref=db.backref('Cars', lazy=False))
 
     def __repr__(self):
-        return f'{self.id}'
+        return f'({self.id_car}, {self.model})'
 
 
 class Transmissions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
 
+    def __repr__(self):
+        return f'({self.id}, {self.name})'
+
 
 class Bodies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'({self.id}, {self.name})'
 
 
 class Engines(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
 
+    def __repr__(self):
+        return f'({self.id}, {self.name})'
+
 
 class Brands(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'({self.id}, {self.name})'
 
 
 class Orders(db.Model):
@@ -58,19 +70,29 @@ class Orders(db.Model):
     call_time = db.Column(db.String(80), default='Now')
     status = db.Column(db.String(80), default='Interested')
     best_price = db.Column(db.Integer, default=1000000000)
+    def __repr__(self):
+        return f'({self.id_order}, {self.id_car}, {self.name_of_customer}, {self.phone})'
 
-
+'''
 # db.create_all()
-brand = Brands(name='BMW')
-transmission = Transmissions(name='Автомат')
-body = Bodies(name='Универсал')
-engine = Engines(name='Дизель')
-car = Cars(model='X5', vin='9876543210', year='2021', colour='white', complectation='speed', photo='link2',
-           price=99999999900, description='Супер крутая тачила', transmission_connection=transmission, engine_connection=engine,
+reno = Cars.query.all()[0]
+bmw = Cars.query.all()[1]
+maserati = Cars.query.all()[2]
+car = Cars(model='GranTurismo', vin='5555555555', year='2020', colour='black', complectation='speed+', photo='link3',
+           price=1050010500, description='Обалдеть, еще быстрее BMW, но не такая комфортная', transmission_connection=obj1, engine_connection=obj2,
            body_connection=body, brand_connection=brand)
 
-db.session.add(car)
+# order1 = Orders(car_connection=reno, name_of_customer='Иван', phone='+77777777777')
+order2 = Orders(car_connection=bmw, name_of_customer='Никита', phone='+79999999999', best_price=12456456564)
+order3 = Orders(car_connection=maserati, name_of_customer='Наталья', phone='+78888888888', status='Сделал заказ', best_price=123456)
+# db.session.add(car)
 # db.session.add(body)
 # db.session.add(engine)
-# db.session.add(car)
+db.session.add_all([order2,order3])
+db.session.commit()
+# print(reno)'''
+bmw = Cars.query.all()[1]
+third_order = Orders.query.all()[2]
+third_order.car_connection = bmw
+db.session.add(third_order)
 db.session.commit()
